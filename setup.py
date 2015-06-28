72c4bf910c68291a41ad3b2a79d71a17b1230d45
williamgilpin <williamgilpin@users.noreply.github.com>
2015-06-28 14:23:10 -0700

    add pypi stuff

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pypdb',
+  packages = ['pypdb'], # same as 'name'
+  version = '0.1',
+  description = 'A Python wrapper for the Protein Data Bank API',
+  author = 'William Gilpin',
+  author_email = 'firstnamelastname(as one word)@googleemailservice',
+  url = 'https://github.com/williamgilpin/pypdb',
+  download_url = 'https://github.com/williamgilpin/pypdb/tarball/0.1', 
+  keywords = ['protein','data','RESTful','api'],
+  classifiers = [],
+)
