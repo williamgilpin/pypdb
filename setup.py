2514208469ac2e51c00078a7f66d3ce3fa51aa83
williamgilpin <williamgilpin@users.noreply.github.com>
2015-08-22 01:27:26 -0700

    update version number:

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name = 'pypdb',
   packages = ['pypdb'], # same as 'name'
-  version = '0.6',
+  version = '1.0',
   install_requires=[
         'xmltodict', 
         'beautifulsoup4'
