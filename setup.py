be8f588324bc60c861cf7d9f9695ec2c8f79d64e
williamgilpin <williamgilpin@users.noreply.github.com>
2016-01-13 00:49:10 -0800

    better setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name = 'pypdb',
   packages = ['pypdb'], # same as 'name'
-  version = '1.101',
+  version = '1.102',
   install_requires=[
         'xmltodict', 
         'beautifulsoup4'
