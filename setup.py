bfb9e1b15b4ad097c5add50c4c176ac6cb28ee15
william <william@DN0a1e2739.SUNet>
2019-03-18 20:38:46 -0700

    update version number

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 setup(
   name = 'pypdb',
   packages = ['pypdb'], # same as 'name'
-  version = '1.2',
+  version = '1.200',
   install_requires=[
         'xmltodict', 
         'beautifulsoup4'
   ],
   description = 'A Python wrapper for the RCSB Protein Data Bank (PDB) API',
   author = 'William Gilpin',
-  author_email = 'firstnamelastname(as one word)@googleemailservice',
+  author_email = 'firstname_lastname@gmail.com',
   url = 'https://github.com/williamgilpin/pypdb',
   download_url = 'https://github.com/williamgilpin/pypdb/tarball/0.6', 
   keywords = ['protein','data','RESTful','api'],
