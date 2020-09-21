79fb49bc6abcd3b3c81cb521c8e5937f22b7d034
william <williamgilpin@gmail.com>
2020-09-21 08:24:16 -0700

    update release and setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name = 'pypdb',
   packages = ['pypdb'], # same as 'name'
-  version = '1.300',
+  version = '1.31',
   install_requires=[
         'xmltodict', 
         'beautifulsoup4',
