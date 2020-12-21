b8e1b54e7890c0f50be531f6d120efc9ba5deff4
lacoperon <elliot.james.williams@gmail.com>
2020-12-21 12:13:11 -0500

    [FASTA 'BLAST' search] - Implemented FASTA file fetcher, sequence autoresolve for the SequenceOperator

--- pypdb/clients/search/operators/sequence_operators.py
+++ pypdb/clients/search/operators/sequence_operators.py
@@ -1,7 +1,7 @@
 """Search operator for searching sequences using MMseqs2 (BLAST-like)."""
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Dict, Union
+from typing import Any, Dict, Optional, Union
 
 class SequenceType(Enum):
     """Type of sequence being searched."""
@@ -9,19 +9,50 @@ class SequenceType(Enum):
     RNA = "pdb_rna_sequence"
     PROTEIN= "pdb_protein_sequence"
 
+class CannotAutoresolveSequenceTypeError(Exception):
+    """Raised when a sequence is ambiguous as to its `SequenceType`."""
+
 
 @dataclass
 class SequenceOperator:
     """Default search operator; searches across available fields search,
     and returns a hit if a match happens in any field."""
     sequence: str
-    sequence_type: SequenceType
+     # If the sequence type is not specified, tries to autoresolve the type from
+     # the sequence itself
+    sequence_type: Optional[SequenceType] = None
     # Maximum E Value allowed for results
     # (see: https://www.ncbi.nlm.nih.gov/BLAST/tutorial/Altschul-1.html)
     evalue_cutoff: float = 100
     # Minimum identity cutoff allowed for results
     # (see: https://www.ncbi.nlm.nih.gov/books/NBK62051/def-item/identity/)
     identity_cutoff: float = 0.95
+
+    def __post_init__(self):
+        if self.sequence_type is None:
+            self._autoresolve_sequence_type()
+
+    def _autoresolve_sequence_type(self):
+        unique_letters = set(list(self.sequence))
+
+        dna_letter_set = set(["A", "T", "C", "G"])
+        rna_letter_set = set(["A", "U", "C", "G"])
+        protein_letter_set = set(list("ABCDEFGHIKLMNPQRSTVWXYZ"))
+        protein_fingerprint_set = set(list("BDEFHIKLMNPQRSVWXYZ"))
+        if unique_letters.issubset(dna_letter_set) and "T" in unique_letters:
+            self.sequence_type = SequenceType.DNA
+        elif unique_letters.issubset(rna_letter_set) and "U" in unique_letters:
+            self.sequence_type = SequenceType.RNA
+        elif (unique_letters.issubset(protein_letter_set) and
+              protein_fingerprint_set & unique_letters):
+            self.sequence_type = SequenceType.PROTEIN
+        else:
+            raise CannotAutoresolveSequenceTypeError(
+                "Sequence is ambiguous as to its SequenceType: `{}`".format(
+                    self.sequence
+                )
+            )
+
 
     def _to_dict(self) -> Dict[str, Any]:
         return {
